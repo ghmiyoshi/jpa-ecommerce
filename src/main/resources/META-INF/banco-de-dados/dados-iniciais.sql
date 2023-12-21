10a6a2e600532aec9826040d77c8203554b92834
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2023-12-20 23:40:30 -0300

    Criando o persistence.xml

---
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -0,0 +1,1 @@
+INSERT INTO produto (id, nome, preco, descricao) VALUES (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
