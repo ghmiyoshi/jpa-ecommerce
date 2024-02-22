6c466904b1119f7d30e52d5b1790f7784eed99b4
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2024-02-21 23:44:20 -0300

    Exportando os scripts de schema generation para arquivos externos

--- src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
+++ src/main/resources/META-INF/banco-de-dados/dados-iniciais.sql
@@ -2,11 +2,11 @@ INSERT INTO produtos (id, nome, preco, data_criacao, descricao) VALUES (1, 'Kind
 
 INSERT INTO clientes (id, nome, cpf) VALUES (1, 'Fernando Medeiros', '123.456.789-10');
 INSERT INTO clientes (id, nome, cpf) VALUES (2, 'Marcos Mariano', '123.456.789-11');
-INSERT INTO clientes_detalhes (id, cliente_id, data_nascimento) VALUES (1, 1, '1990-01-01');
-INSERT INTO clientes_detalhes (id, cliente_id, data_nascimento) VALUES (2, 2, '1990-01-01');
+INSERT INTO clientes_detalhes (cliente_id, data_nascimento) VALUES (1, '1990-01-01');
+INSERT INTO clientes_detalhes (cliente_id, data_nascimento) VALUES (2, '1990-01-01');
 
 INSERT INTO pedidos (id, cliente_id, data_pedido, total, status) VALUES (1, 1, sysdate(), 998.0, 'AGUARDANDO');
-INSERT INTO pedidos (id, cliente_id, data_pedido, total, status) VALUES (2, 1, sysdate(), 499.0, 'AGUARDANDO');
+INSERT INTO pedidos (id, cliente_id, data_pedido, total, status) VALUES (2, 2, sysdate(), 499.0, 'AGUARDANDO');
 INSERT INTO item_pedidos (pedido_id, produto_id, preco_produto, quantidade) VALUES (1, 1, 499, 2);
 INSERT INTO item_pedidos (pedido_id, produto_id, preco_produto, quantidade) VALUES (2, 1, 499, 1);
 
