15b824cfc5ed71913beee09576c8e5480dc5ff0d
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2024-03-18 22:44:21 -0300

    Entendendo as diferenças entre TypedQuery e Query

--- src/test/java/jpql/BasicoJPQLTest.java
+++ src/test/java/jpql/BasicoJPQLTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.algaworks.ecommerce.EntityManagerTest;
 import com.algaworks.ecommerce.models.Pedido;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 import org.junit.jupiter.api.Test;
 
@@ -13,13 +14,29 @@ class BasicoJPQLTest extends EntityManagerTest {
   void buscarPorIdentificador() {
     // entityManager.find(Pedido.class, 1)
 
-    TypedQuery<Pedido> typedQuery = entityManager
-        .createQuery("select p from Pedido p where p.id = 1", Pedido.class);
+    TypedQuery<Pedido> typedQuery = entityManager.createQuery(
+        "select p from Pedido p where p.id = 2", Pedido.class);
 
     final var pedido = typedQuery.getSingleResult();
     assertNotNull(pedido);
 
     // List<Pedido> lista = typedQuery.getResultList();
+    // assertFalse(lista.isEmpty());
+  }
+
+  @Test
+  void mostrarDiferencaQueries() {
+    String jpql = "select p from Pedido p where p.id = 1";
+
+    TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+    final var pedido1 = typedQuery.getSingleResult();
+    assertNotNull(pedido1);
+
+    Query query = entityManager.createQuery(jpql);
+    Pedido pedido2 = (Pedido) query.getSingleResult();
+    assertNotNull(pedido2);
+
+    // List<Pedido> lista = query.getResultList();
     // assertFalse(lista.isEmpty());
   }
 }
