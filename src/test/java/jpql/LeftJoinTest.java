2ba0a5f9362d30113bc9767bfef0f9c3d552940f
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2024-04-18 22:56:47 -0300

    Usando left outer join

---
+++ src/test/java/jpql/LeftJoinTest.java
@@ -0,0 +1,52 @@
+package jpql;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.models.Pedido;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class LeftJoinTest extends EntityManagerTest {
+
+    @Test
+    void fazerJoin() {
+        final var jpql = "select p from Pedido p left join p.pagamento pag where pag.status = 'PROCESSANDO'";
+
+        List<Pedido> pedidos = entityManager.createQuery(jpql, Pedido.class).getResultList();
+
+        assertFalse(pedidos.isEmpty());
+        assertEquals(2, pedidos.size());
+    }
+
+    @Test
+    void fazerJoin2() {
+        final var jpql = "select p, pag from Pedido p left join p.pagamento pag";
+
+        List<Object[]> results = entityManager.createQuery(jpql, Object[].class).getResultList();
+
+        assertFalse(results.isEmpty());
+        assertEquals(2, results.size());
+    }
+
+    @Test
+    void fazerJoin3() {
+        final var jpql = "select p from Pedido p left join p.itens i";
+
+        List<Object[]> results = entityManager.createQuery(jpql, Object[].class).getResultList();
+
+        assertFalse(results.isEmpty());
+        assertEquals(2, results.size());
+    }
+
+    @Test
+    void fazerJoinComJoin() {
+        final var jpql = "select prod from Pedido p left join p.itens i join i.produto prod";
+
+        List<Object[]> results = entityManager.createQuery(jpql, Object[].class).getResultList();
+
+        assertFalse(results.isEmpty());
+        assertEquals(2, results.size());
+    }
+}
