287c1c4ebb63d6057e50c1e6ac330753e2591e89
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2024-04-26 19:58:16 -0300

    Consultando pedidos com produto específico

--- src/test/java/jpql/PathExpressionTest.java
+++ src/test/java/jpql/PathExpressionTest.java
@@ -9,8 +9,18 @@ import org.junit.jupiter.api.Test;
 class PathExpressionTest extends EntityManagerTest {
 
     @Test
-    void pathExpression() {
+    void usarPathExpressions() {
         final var jpql = "select p.cliente.nome from Pedido p";
+        final var pedidos = entityManager.createQuery(jpql, Pedido.class).getResultList();
+
+        assertFalse(pedidos.isEmpty());
+    }
+
+    @Test
+    void buscarPedidosComProdutoEspecifico() {
+        final var jpql = "select p from Pedido p join p.itens i where i.id.produtoId = 1";
+        // String jpql = "select p from Pedido p join p.itens i where i.produto.id = 1";
+       // String jpql = "select p from Pedido p join p.itens i join i.produto pro where pro.id = 1";
         final var pedidos = entityManager.createQuery(jpql, Pedido.class).getResultList();
 
         assertFalse(pedidos.isEmpty());
