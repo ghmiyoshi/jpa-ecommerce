166de307c14254a3e9759ccb4041643bcd3021ae
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2024-05-30 00:01:05 -0300

    Fazendo paginação de resultados

---
+++ src/test/java/jpql/PaginacaoJPQLTest.java
@@ -0,0 +1,28 @@
+package jpql;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.models.Categoria;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class PaginacaoJPQLTest extends EntityManagerTest {
+
+    @Test
+    void paginarResultados() {
+        String jpql = "select c from Categoria c order by c.nome";
+
+        TypedQuery<Categoria> typedQuery = entityManager.createQuery(jpql, Categoria.class);
+
+        // FIRST_RESULT = MAX_RESULTS * (pagina - 1)
+        typedQuery.setFirstResult(6);
+        typedQuery.setMaxResults(2);
+
+        List<Categoria> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+
+        lista.forEach(c -> System.out.println(c.getId() + ", " + c.getNome()));
+    }
+}
