3000d9b84652ee1d6eacbffb88d8afee96f9c427
Gabriel Hideki <gabriel_hidekii@hotmail.com>
2024-06-09 20:19:13 -0300

    Usando funções para datas

--- src/test/java/jpql/FuncoesTest.java
+++ src/test/java/jpql/FuncoesTest.java
@@ -1,9 +1,8 @@
 package jpql;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import com.algaworks.ecommerce.EntityManagerTest;
-import jakarta.persistence.TypedQuery;
-import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FuncoesTest extends EntityManagerTest {
@@ -11,14 +10,27 @@ class FuncoesTest extends EntityManagerTest {
     @Test
     public void aplicarFuncao() {
         // concat, length, locate, substring, lower, upper, trim
-        String jpql = "select c.nome, length(c.nome) from Categoria c " +
+        final var jpql = "select c.nome, length(c.nome) from Categoria c " +
                 " where substring(c.nome, 1, 1) = 'N'";
 
-        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        final var lista = entityManager.createQuery(jpql, Object[].class).getResultList();
 
-        List<Object[]> lista = typedQuery.getResultList();
-        Assertions.assertFalse(lista.isEmpty());
-
+        assertFalse(lista.isEmpty());
         lista.forEach(arr -> System.out.println(arr[0] + " - " + arr[1]));
+    }
+
+    @Test
+    public void aplicarFuncaoData() {
+        // TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        // current_date, current_time, current_timestamp
+        // year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao)
+
+        final var jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) "
+                + " from Pedido p where hour(p.dataCriacao) > 18";
+
+        final var lista = entityManager.createQuery(jpql, Object[].class).getResultList();
+
+        assertFalse(lista.isEmpty());
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
     }
 }
